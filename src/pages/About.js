be2b897d58ce67af2fe7131fadaac2cdba9afb24
Kristen <kristenwood123@gmail.com>
2021-06-10 11:18:12 -0700

    added about and error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h1>about</h1>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo facere, placeat facilis vel praesentium porro rem ipsam ducimus dignissimos ullam vero possimus nam quod, veritatis laborum aliquam! Perspiciatis inventore maxime tenetur molestias quaerat numquam, quae debitis commodi a totam. Aspernatur!</p>
+    </section>
   )
 }
 
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <section className='error-page section'>
+      <div className="error-container">
+        <h1>Opps! it's a dead end</h1>
+        <Link to='/' className='btn btn-primary'>
+          back home
+        </Link>
+      </div>
+    </section>
   )
 }
 
