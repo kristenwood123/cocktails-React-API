e9c27d6f553885a8b98df4ddfe95ac2d43da6f70
Kristen <kristenwood123@gmail.com>
2021-06-12 16:35:58 -0700

    added fetch to api for single cocktail

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Loading from '../components/Loading'
+import { useParams, Link } from 'react-router-dom'
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
+  const { id } = useParams()
+  const [loading, setLoading] = useState(false)
+  const [cocktail, setCocktail] = useState(null)
+
+
+  useEffect(() => {
+    setLoading(true)
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`)
+        const data = await response.json()
+        if(data.drinks) {
+
+        } else {
+          setCocktail(null)
+        }
+        setLoading(false)
+      }
+      catch(e) {
+        console.log(e);
+        setLoading(false)
+      }
+    }
+    getCocktail()
+  },[id])
   return (
-    <div>
-      
-    </div>
+    <section>
+      <h1>{id}</h1>
+    </section>
   )
 }
 
