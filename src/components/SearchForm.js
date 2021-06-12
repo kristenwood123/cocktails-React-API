bcedd1f77473208e02385a68aad340289aa8dfbd
Kristen <kristenwood123@gmail.com>
2021-06-11 17:17:53 -0700

    completed search form with useRef and focus

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,12 +6,25 @@ const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext()
   const searchValue = React.useRef('')
 
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  React.useEffect(() => {
+    searchValue.current.focus()
+  },[])
+
   return (
     <section className='section search'>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name"> search your favorite cocktail </label>
-          <input type="text" id='name' ref={searchValue} />
+          <input type="text" id='name' ref={searchValue} onChange={searchCocktail}/>
+          
         </div>
       </form>
     </section>
